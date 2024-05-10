f4be54e80480617a58633d5f70ae48a6ff7f81de
hiewbt <hieubt.bk66@gmail.com>
2024-05-11 02:26:24 +0700

    update sign in page

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,8 +1,10 @@
-function Input({ label, type }) {
+import "../Input/index.css"
+
+function Input({ label, type, id }) {
   return (
-    <div className="bg relative mx-auto w-200 border-2 py-4">
-      <input type="text" id="input" required="" className="focus:outline-none focus:ring-black focus:ring-opacity-50 peer rounded-lg px-3 text-gray-700" />
-      <label htmlFor="input" className="absolute top-0 left-0 text-gray-400 peer-focus:top-[-20px] peer-focus:text-gray-700 peer-focus:font-medium transition duration-300 ease-in-out">Enter Text</label>
+    <div className="input-box">
+      <input required type={type} id={id} className="input" />
+      <label htmlFor={id} className="label text--flicking">{label}</label>
     </div>
   )
 }
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,19 +1,41 @@
 import bgImage from '../../assets/Untitled.png'
 import Input from '../../components/Input'
+import SubmitButton from '../../components/SubmitButton/SubmitButton'
 import Logo from '../../components/Svg/Logo'
+import {Facebook, Apple, Google} from '../../components/Svg/SignLogo'
 
 function SignIn() {
   return (
     <>
       <div className="flex flex-row h-screen w-screen">
-        <div className="w-1/4 h-full bg-gray-200 flex flex-col justify-center">
-          <div className="">
-            <Logo />
-          </div>
-          <div>
-            <form>
-              <Input label={"username"} type={"text"} />
-            </form>
+        <div className='w-1/4 flex justify-center align-middle'>
+          <div className="h-full flex flex-col justify-center">
+            <div className="mb-8">
+              <Logo />
+            </div>
+            <div>
+              <form className=''>
+                <Input label={"username"} type={"text"} id={'1'}/>
+                <Input label={"password"} type={"password"} id={'2'} />
+                <div className='flex justify-between'>
+                  <div className=' bg-blue-500 w-20 py-1 rounded-md flex justify-center'>
+                    <Facebook />
+                  </div>
+                  <div className=' bg-black w-20 py-1 rounded-md flex justify-center'>
+                    <Apple />
+                  </div>
+                  <div className='border-black border-[1px] w-20 py-1 rounded-md flex justify-center'>
+                    <Google />
+                  </div>
+                </div>
+                <div className='mt-2 align-middle'>
+                  <input id='checkbox' type="checkbox" className='w-4 h-4 mr-5'/>
+                  <label htmlFor="checkbox">stay signed in</label>
+                </div>
+                <SubmitButton />
+
+              </form>
+            </div>
           </div>
         </div>
         <div className="w-3/4 h-full bg-slate-600">
