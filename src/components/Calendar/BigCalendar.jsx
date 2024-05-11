d65b99b1464964421b91da8e1719cc6d7aee0768
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2024-05-11 22:28:08 +0700

    them hieu ung click chuot cho event

--- src/components/Calendar/BigCalendar.jsx
+++ src/components/Calendar/BigCalendar.jsx
@@ -4,16 +4,31 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin from "@fullcalendar/interaction"
 import { useRef } from 'react';
-export default function BigCalendar() {
+export default function BigCalendar({setClickedDate, setEvents}) {
     var events = [
-        { title: 'Event Title', start: '2024-05-20' }, 
-        { title: 'Meeting', start: '2024-05-22T10:00:00', end: '2024-05-22T11:00:00' } 
+        { title: 'Event Title', start: '2024-05-20', id: '1' }, 
+        { title: 'Meeting', start: '2024-05-22T10:00:00', end: '2024-05-22T11:00:00' , id: '2'} 
     ];
     
     const calendarRef =  useRef(null)
 
-    const handleDateClick = () => {
-      
+
+
+    const handleDateClick = (info) => {
+      var date = new Date(info.dateStr)
+      setClickedDate(date.toDateString())
+      var events = calendarRef.current.getApi().getEvents()
+      console.log(events)
+      var result = []
+      for(let index in events) {
+          var start = events[index]._instance.range.start.getDate()
+          var end = events[index]._instance.range.end.getDate()
+          if(start <= date.getDate() && date.getDate() <= end) {
+              console.log(start <= date.getDate() && date.getDate() <= end)
+              result.push(events[index])
+          }
+      }
+      setEvents(result);
     }
 
   return (
