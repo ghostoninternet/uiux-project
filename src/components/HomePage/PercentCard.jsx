b3c4d3866ab524a91a1792d841e585de879d77b9
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2024-05-17 14:23:29 +0700

    nothasda

--- src/components/HomePage/PercentCard.jsx
+++ src/components/HomePage/PercentCard.jsx
@@ -7,7 +7,7 @@ function PercentCard({ title, value }) {
   
   return (
     <div className="p-7 w-1/3 bg-white min-h-36 shadow-lg rounded-xl flex flex-col justify-between relative">
-      <div className="big-circle w-36 h-36 top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%]" 
+      <div className="big-circle w-36 h-36 top-[55%] translate-y-[-50%] left-[50%] translate-x-[-50%]" 
           style={{
               backgroundImage: `conic-gradient(
                   #FABB18 0deg, #FABB18 ${KPIcompletedDeg}deg,
@@ -21,7 +21,7 @@ function PercentCard({ title, value }) {
       <h1 className="font-bold text-xl">{title}</h1>
       <ShowMore />
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-5">
         <div className="p-2 bg-[#FABB18] rounded-lg">
           <svg width="30" height="30" viewBox="0 0 24 24" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 3V21M12 3L9 6M12 3L15 6M12 21L15 18M12 21L9 18M3 12H21M3 12L6 15M3 12L6 9M21 12L18 9M21 12L18 15" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
