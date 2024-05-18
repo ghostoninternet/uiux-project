2f22aa6fd970dc3bea20422dcd693cdf13570f1f
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2024-05-18 11:32:48 +0700

    abc

--- src/pages/Setting/components/Tab.jsx
+++ src/pages/Setting/components/Tab.jsx
@@ -3,7 +3,7 @@ function Tab({ selectedTab, setSelectedTab, tabTitle }) {
   return (
     <div 
       onClick={() => setSelectedTab(tabTitle)}
-      className="cursor-pointer px-5 py-2 text-xl"
+      className="cursor-pointer px-5 py-2 text-xl hover:bg-[#E0E0E0] mb-1 rounded-[1rem]"
       style={{
         fontWeight: selectedTab === tabTitle ? '600' : undefined,
         backgroundColor: selectedTab === tabTitle ? '#D9D9D9': undefined,
