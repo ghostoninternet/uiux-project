e4c8e03d103fce08093d22c9a5d16c68ee579ce4
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2024-05-23 22:07:42 +0700

    set up mobile layout

---
+++ src/Mobile.jsx
@@ -0,0 +1,12 @@
+
+import 'react-toastify/dist/ReactToastify.css';
+function Mobile() {
+
+  return (
+   <div>
+        acs
+   </div>
+  )
+}
+
+export default Mobile
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Mobile from './Mobile.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function ResponsiveApp() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 640);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <React.StrictMode>
+      {isMobile ? <Mobile /> : <App />}
+    </React.StrictMode>
+  );
+}
+
+root.render(<ResponsiveApp />);
